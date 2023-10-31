467d73f46ffdda7b3ed4076c3cf0fbfe5d7ae385
Pablo Lima Dias <pabloidz@gmail.com>
2023-10-30 22:37:41 -0300

    Changes to main.tf (#4)

--- main.tf
+++ main.tf
@@ -1,17 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_db_instance" "example" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "15.3"
-  instance_class       = "db.t3.micro"
-  name                 = "database"
-  username             = "dbuser"
-  password             = "dbpassword"
-  parameter_group_name = "default.postgres15"
+resource "aws_db_instance" "soat1" {
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "15.3"
+  instance_class          = "db.t3.micro"
+  username                = "dbuser"
+  password                = "dbpassword"
+  parameter_group_name    = "default.postgres15"
+  skip_final_snapshot     = true
+  backup_retention_period = 0
+  apply_immediately       = true
 
   tags = {
     Name = "database"
